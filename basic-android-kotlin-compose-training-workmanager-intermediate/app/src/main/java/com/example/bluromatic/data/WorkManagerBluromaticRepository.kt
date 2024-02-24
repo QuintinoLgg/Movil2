819a2b25e27f11bcfaf916e2639db8de15431e7c
QuintinoLgg <s20120151@alumnos.itsur.edu.mx>
2024-02-24 11:36:41 -0600

    Restricciones de trabajo

--- basic-android-kotlin-compose-training-workmanager-intermediate/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ basic-android-kotlin-compose-training-workmanager-intermediate/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,6 +19,7 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.asFlow
+import androidx.work.Constraints
 import androidx.work.Data
 import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
@@ -52,6 +53,12 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
      * @param blurLevel The amount to blur the image
      */
     override fun applyBlur(blurLevel: Int) {
+        val constraints = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .build()
+
+
+
         var continuation = workManager
             .beginUniqueWork(
                 IMAGE_MANIPULATION_WORK_NAME,
@@ -64,6 +71,8 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
         // Input the Uri for the blur operation along with the blur level
         blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
+
+        blurBuilder.setConstraints(constraints) // Add this code
 
         continuation = continuation.then(blurBuilder.build())
 
